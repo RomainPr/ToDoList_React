f29a0040d795efeae919b259a3cc3410a3e9b488
Rom <romainprevostdev@gmail.com>
2021-05-07 13:25:57 +0200

    Adding createRef() as example to put a focus on input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,18 +3,41 @@ import PropTypes from 'prop-types';
 
 import './form.scss';
 
-const Form = ({ baseValue, onInputChange, onFormSubmit }) => (
-  <form className="form" onSubmit={onFormSubmit}>
-    <input
-      type="text"
-      className="form-item"
-      placeholder="Ajouter une tâche"
-      value={baseValue}
-      onChange={(event) => onInputChange(event.target.value)}
-      onSubmit={(event) => onFormSubmit(event.preventDefault())}
-    />
-  </form>
-);
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // on commence pas créer la référence, dans le constructeur
+    this.taskInput = React.createRef();
+  }
+
+  componentDidMount() {
+    // la ref est accessible après le rendu
+    // c'est une référence (et oui) vers l'élément du DOM réel
+    // la référence sera automatiquement mise à jour par React
+    // lors de chaque nouveau rendu.
+    this.taskInput.current.focus();
+  }
+
+  render() {
+    const { baseValue, onInputChange, onFormSubmit } = this.props;
+
+    return (
+      <form className="form" onSubmit={onFormSubmit}>
+        <input
+        // la ref sera remplie lors du render
+          ref={this.taskInput}
+          type="text"
+          className="form-item"
+          placeholder="Ajouter une tâche"
+          value={baseValue}
+          onChange={(event) => onInputChange(event.target.value)}
+          onSubmit={(event) => onFormSubmit(event.preventDefault())}
+        />
+      </form>
+    );
+  }
+}
 
 Form.propTypes = {
   baseValue: PropTypes.string.isRequired,
